916b31940080e2961104d7d5cb3bf26e14c220b1
nguyendangphuhero <nguyendangphu1234567890@gmail.com>
2022-04-08 08:07:37 +0700

    pomodoro

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import TodoList from './components/TodoList';
+import Pomodoro from './components/Pomodoro/App'
 
 function App() {
   return (
     <div className='todo-app'>
       <TodoList />
+      <Pomodoro></Pomodoro>
     </div>
   );
 }
